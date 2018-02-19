198a5e4bda4d19e9209b993ad76fd22e90ebc964
Pratik Keny <pratikkeny4@gmail.com>
2018-02-18 21:55:20 -0500

    local

--- Gemfile
+++ Gemfile
@@ -28,7 +28,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+ gem 'bcrypt',git: 'https://github.com/codahale/bcrypt-ruby.git', :require => 'bcrypt'
+ gem 'coffee-script-source','1.8.0'
+ gem 'devise'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users do
+    get "sign_up", to: "devise/registrations#new"
+    get "sign_in", to: "devise/sessions#new"
+    #root "devise/sessions#new"
+    #get "sign_out", to: "devise/sessions#destroy"
+  end
+  resources :homes
+  #root "users#index"
+  #root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'devise/sessions#new'
 end
